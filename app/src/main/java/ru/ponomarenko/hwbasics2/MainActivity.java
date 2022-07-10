7014b7c8b68f36bd31df609fd463a4cc7a1d42a6
Ponomarenko <firebird89@list.ru>
2022-07-11 00:20:17 +0300

    Готово, без исправления глюка с переворотом

--- app/src/main/java/ru/ponomarenko/hwbasics2/MainActivity.java
+++ app/src/main/java/ru/ponomarenko/hwbasics2/MainActivity.java
@@ -2,6 +2,7 @@ package ru.ponomarenko.hwbasics2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+
+            NotesFragment notesFragment = new NotesFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container_edit, NoteEditFragment.newInstance(1))
+                    .commit();
+
+        } else {
+            NotesFragment notesFragment = new NotesFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, notesFragment)
+                    .commit();
+        }
+
     }
 }
