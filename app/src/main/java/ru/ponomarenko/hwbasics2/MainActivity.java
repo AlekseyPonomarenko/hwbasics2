8fb89240c5705f4309b80d11d781cef1442083c4
Ponomarenko <firebird89@list.ru>
2022-07-13 22:02:02 +0300

    Урок 7. Фрагменты — продолжение
    Добавлен функционал добавления/редактирования/удаления
    Передача между фрагментами с помощью parcelable
    Колбэк сформирвоал не совсем стандартным сопособом. Пример что приводися на уроке не взлетел. Пришлось импровизировать. Но заработало.

--- app/src/main/java/ru/ponomarenko/hwbasics2/MainActivity.java
+++ app/src/main/java/ru/ponomarenko/hwbasics2/MainActivity.java
@@ -5,28 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.res.Configuration;
 import android.os.Bundle;
 
+import ru.ponomarenko.hwbasics2.service.MainService;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Создаем демо лист
+        MainService.getInstance().generateDemoList();
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        NotesFragment notesFragment = new NotesFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, notesFragment)
+                .commit();
+
+        /*
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
 
             NotesFragment notesFragment = new NotesFragment();
             getSupportFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.fragment_container_edit, NoteEditFragment.newInstance(1))
+                    .replace(R.id.fragment_container_edit, NoteEditFragment.newInstance("1"))
                     .commit();
 
         } else {
-            NotesFragment notesFragment = new NotesFragment();
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.fragment_container, notesFragment)
-                    .commit();
+
         }
+
+         */
 
     }
 }
---
+++ app/src/main/java/ru/ponomarenko/hwbasics2/service/MainService.java
@@ -0,0 +1,54 @@
+package ru.ponomarenko.hwbasics2.service;
+
+import java.util.ArrayList;
+
+import ru.ponomarenko.hwbasics2.model.Note;
+import ru.ponomarenko.hwbasics2.repo.NoteRepo;
+import ru.ponomarenko.hwbasics2.repo.NoteRepoImpl;
+
+public class MainService {
+
+    /**
+     * Синглтон
+     */
+    private static MainService INSTANCE;
+
+    private MainService() {
+
+        noteList = new ArrayList<Note>();
+        noteRepo = new NoteRepoImpl(noteList);
+
+    }
+
+    public static synchronized MainService getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new MainService();
+        }
+        return INSTANCE;
+    }
+
+    /**
+     * Описание общедоступных свойств
+     */
+    private NoteRepo noteRepo;
+    private ArrayList<Note> noteList;
+
+    /**
+     * Демо генерация списка
+     */
+    public void generateDemoList() {
+        if (!noteList.isEmpty()) {
+            return;
+        };
+
+        for (int i = 1; i<11; i++){
+            noteRepo.createOrUpdate(new Note("Тема " + i, "Детальное описание темы " + i));
+        }
+
+
+    }
+
+    public NoteRepo getNoteRepo() {
+        return noteRepo;
+    }
+}
